3fa58c1734f5ad270aa5c96b6a007b690f0a64d8
NeginSal <neginsaljooghi@gmail.com>
2021-09-13 17:27:34 +0430

    change Detail.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,25 +1,36 @@
 import { useEffect, useState } from "react";
+import { getMovie } from "./Transportlayer";
 
-function getServerData(id) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Film #" + id);
-    }, 3000);
-  });
-}
-
-const Details = ({ id }) => {
+const Details = ({ id,onClickBack }) => {
   const [Loading, setLoading] = useState(true);
+  const [name, setName] = useState("");
   const [description, setdescription] = useState("");
 
   useEffect(() => {
-    getServerData(id).then((data) => {
-      setdescription(data);
+    fetchAndUpdate(id);
+  }, []);
+
+  function fetchAndUpdate(id) {
+    setLoading(true);
+    getMovie(id).then((data) => {
+      setName(data.name);
+      setdescription(data.description);
       setLoading(false);
     });
-  });
+  }
 
-  return <div>{Loading ? "Please wait..." : description}</div>;
+  return <div>
+    Loading ? (<Loading/>): (
+      <div>
+        <h3>Detail</h3>
+        <div>
+          <div>{`Name:${name}`}</div>
+          <div>{`Description:${description}`}</div>
+          <button onClick={onClickBack}>Back</button>
+        </div>
+      </div>
+    )
+    </div>;
 };
 
 export default Details;
