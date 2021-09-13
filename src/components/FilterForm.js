93f26dc9401262b239cbc4ced1a0d49a45c863e6
NeginSal <neginsaljooghi@gmail.com>
2021-09-13 16:30:18 +0430

    add filter form and loading component

---
+++ src/components/FilterForm.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+const FilterForm = ({ Keyword, OverSeven, onChangeInputs }) => {
+  return (
+    <div>
+      <div>
+        Keyword
+        <input
+          type="text"
+          value={Keyword}
+          onChange={(e) =>
+            onChangeInputs({ value: e.target.value, name: e.target.value })
+          }
+          name="Keyword"
+        />
+      </div>
+      <div>
+        Only over 7.0
+        <input
+          type="checkbox"
+          checked={OverSeven}
+          onChange={(e) =>
+            onChangeInputs({ value: e.target.checked, name: e.target.name })
+          }
+          name="overseven"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FilterForm;
